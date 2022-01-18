0e20958fc5177b42bed8e12a74100b55a4e07a01
avneet36 <95776606+avneet36@users.noreply.github.com>
2022-01-18 15:49:17 -0800

    teamcode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
@@ -4,12 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-@TeleOp(name = "motorControl", group = "TeleOp")
+@TeleOp(name = "FinalTeleOp", group = "TeleOp")
 
 public class FinalTeleOp extends LinearOpMode {
 
@@ -26,6 +26,7 @@ public class FinalTeleOp extends LinearOpMode {
 
     double motorPower = (0.5);
 
+
     @Override
     public void runOpMode() {
         //mapping
@@ -33,11 +34,11 @@ public class FinalTeleOp extends LinearOpMode {
         rightDrive = hardwareMap.get(DcMotor.class, "motor2");
         armMotor1 = hardwareMap.get(DcMotor.class, "motor3");
         armMotor2 = hardwareMap.get(DcMotor.class, "motor4");
-        crServo = hardwareMap.get(CRServo.class, "servo1");
-        clawServo = hardwareMap.get(Servo.class, "servo2");
+        crServo = hardwareMap.get(CRServo.class, "servo2");
+        clawServo = hardwareMap.get(Servo.class, "servo1");
         topLimitSwitch1 = hardwareMap.get(DigitalChannel.class, "digital_touch1");
         //topLimitSwitch2 = hardwareMap.get(DigitalChannel.class, "digital_touch2");
-        bottomLimitSwitch1 = hardwareMap.get(DigitalChannel.class, "digital_touch3");
+        bottomLimitSwitch1 = hardwareMap.get(DigitalChannel.class, "digital_touch2");
         //getBottomLimitSwitch2= hardwareMap.get(DigitalChannel.class, "digital_touch4");
 
 
@@ -49,17 +50,13 @@ public class FinalTeleOp extends LinearOpMode {
         clawServo.setDirection(Servo.Direction.FORWARD);
         crServo.setDirection(CRServo.Direction.FORWARD);
 
-        //power
-
-        double armPower = (0.65);
-
-        //position
-        clawServo.setPosition(0);
+        //servo
+        clawServo.setPosition(1);
+        clawServo.scaleRange(0.75, 1);
 
         //LimitSwitch
         topLimitSwitch1.setMode(DigitalChannel.Mode.INPUT);
         bottomLimitSwitch1.setMode(DigitalChannel.Mode.INPUT);
-
 
         waitForStart();
 
@@ -71,25 +68,70 @@ public class FinalTeleOp extends LinearOpMode {
 
 
         while (opModeIsActive()) {
-
-            chaseeDrive();
+            chasisDrive();
             armControl();
             limitSwitch();
+            claw();
+            telemetry.addData("Velocity", ((DcMotorEx) armMotor1).getVelocity());
+            telemetry.addData("Top", topLimitSwitch1.getState());
+            telemetry.addData("Bottom ", bottomLimitSwitch1.getState());
+            telemetry.addData("Position", clawServo.getPosition());
 
             telemetry.update();
 
-            }
         }
-    private void limitSwitch() {
+    }
+
+    private void chasisDrive() {
         leftDrive.setPower(-gamepad1.left_stick_x * motorPower + gamepad1.left_stick_y * motorPower);
         rightDrive.setPower(gamepad1.left_stick_x * motorPower + gamepad1.left_stick_y * motorPower);
         telemetry.addData("leftStickPositionx", gamepad1.left_stick_x);
         telemetry.addData("leftStickPositionY", gamepad1.left_stick_y);
     }
-    private void armControl(){
+
+    private void armControl() {
+        if (gamepad1.y) {
+            ((DcMotorEx) armMotor1).setVelocity(1500);
+            ((DcMotorEx) armMotor2).setVelocity(1500);
+        } else if (gamepad1.a) {
+            ((DcMotorEx) armMotor1).setVelocity(-1500);
+            ((DcMotorEx) armMotor2).setVelocity(-1500);
+        } else if (gamepad1.b) {
+            ((DcMotorEx) armMotor1).setVelocity(0);
+            ((DcMotorEx) armMotor2).setVelocity(0);
+        }
 
     }
-    private void chaseeDrive(){
+
+    private void limitSwitch() {
+        if (topLimitSwitch1.getState() == true && bottomLimitSwitch1.getState() == false) ;
+        {
+            ((DcMotorEx) armMotor1).setVelocity(-240);
+            ((DcMotorEx) armMotor2).setVelocity(-240);
+            sleep(250);
+            ((DcMotorEx) armMotor1).setVelocity(0);
+            ((DcMotorEx) armMotor2).setVelocity(0);
+            telemetry.update();
+        }
+        if (topLimitSwitch1.getState() == false && bottomLimitSwitch1.getState() == true) {
+            ((DcMotorEx) armMotor2).setVelocity(240);
+            ((DcMotorEx) armMotor1).setVelocity(240);
+            sleep(250);
+            ((DcMotorEx) armMotor2).setVelocity(0);
+            ((DcMotorEx) armMotor1).setVelocity(0);
+            telemetry.update();
+        } else if (topLimitSwitch1.getState() == true && bottomLimitSwitch1.getState() == true) {
+            ((DcMotorEx) armMotor2).setVelocity(0);
+            ((DcMotorEx) armMotor1).setVelocity(0);
+            telemetry.update();
+        }
+    }
+    private void claw(){
+        clawServo.setPosition(1- gamepad1.right_trigger);
+        telemetry.addData("Position", clawServo.getPosition());
 
     }
+
 }
+
+
